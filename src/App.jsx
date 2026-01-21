35cb71b2bc2e6f1b4b3769e1657ac5576026e361
Cristiano Abilio <cristianocafr@gmail.com>
2026-01-21 15:34:25 -0300

    Installed and configure react, tailwind and router

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { Link, Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Welcome from "./compoments/Welcome";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+function App() {
+  return (
+    <Router>
+      <nav>
+        <Link to="/">Home</Link> {" | "}
+        <Link to="/about">About</Link> {" | "}
+        <Link to="/contact">Contact</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
+  )
+}
+
+export default App;
