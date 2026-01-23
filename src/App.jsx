2960dd56584d2e69c656d4f584d0121df1bdc6db
Cristiano Abilio <cristianocafr@gmail.com>
2026-01-23 12:37:46 -0300

    feat: creating the dashboard layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,34 @@
-import { Link, Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { Link, Route, BrowserRouter as Router, Routes, useLocation } from "react-router-dom";
 import Home from "./pages/Home";
-import NavBar from "./compoments/NavBar";
+import NavBar from "./components/NavBar";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import Dashboard from "./pages/Dashboard";
+
+function Layout() {
+  const location = useLocation();
+
+  const showNavBar = ["/","/login","/register"].includes(location.pathname);
+
+  console.log(location.pathname);
+  console.log(showNavBar);
+  return(
+    <>
+      {showNavBar && <NavBar />}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+    </>
+  );
+}
 
 function App() {
   return (
     <Router>
-      <NavBar />
-
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-      </Routes>
+      <Layout />
     </Router>
   )
 }
