8bc65dd9668aec38d5c69d37c7f4289dea96b073
Cristiano Abilio <cristianocafr@gmail.com>
2026-01-28 11:05:03 -0300

    feat: frontend projects

---
+++ src/pages/EditProject.jsx
@@ -0,0 +1,132 @@
+import React, { useEffect, useState } from "react";
+import api from "../axios";
+import DashboardLayout from "../components/DashboardLayout";
+import { useNavigate, useParams } from "react-router-dom";
+
+export default function EditProject() {
+    const {id} = useParams();
+    const [title, setTitle] = useState("");
+    const [description, setDescription] = useState("");
+    const [dueDate, setDueDate] = useState("");
+    const [loading, setLoading] = useState(false);
+    const navigate = useNavigate();
+
+    // fetch project data from backend API
+    useEffect(() => {
+        const fetchProject = async () => {
+            try {
+                const token = localStorage.getItem('token');
+                const response = await api.get(`/projects/${id}`, {
+                    headers : {
+                        Authorization: `Bearer ${token}`,
+                    }
+                });
+
+                setTitle(response.data.data.name);
+                setDescription(response.data.data.description);
+                setDueDate(response.data.data.due_date);
+                console.log(response.data.data);
+            } catch (error) {
+                console.log("error loading project data", error);
+            }
+        }
+
+        fetchProject();
+    }, [id]);
+
+    const handleUpdate = async (e) => {
+        e.preventDefault();
+        setLoading(true);
+
+        try {
+            const token = localStorage.getItem('token');
+            await api.put(`/projects/${id}`, {
+                name: title,
+                description: description,
+                due_date: dueDate
+            },
+            {
+                headers: {
+                    Authorization: `bearer ${token}`
+                }
+            }
+            );
+
+            alert("Project updated successfully.");
+            navigate('/projects');
+        } catch (error) {
+            console.log("Error saving project.", error);
+            alert("failed to add project");
+        } finally {
+            setLoading(false);
+        }
+    }
+
+  return (
+    <DashboardLayout>
+      <div className="p-6 bg-gray-50 min-h-screen">
+        <div className="w-full bg-white shadow-md rounded-lg p-6">
+          <h2 className="text-2xl font-semibold text-gray-800 mb-6">
+            Edit Project
+          </h2>
+
+          <form className="space-y-4 w-full" onSubmit={handleUpdate}>
+            {/* Title */}
+            <div>
+              <label className="block text-sm font-medium text-gray-700">
+                Title
+              </label>
+              <input
+                type="text"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+                placeholder="Enter project title"
+                className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-200 p-2"
+                required
+              />
+            </div>
+
+            {/* Description */}
+            <div>
+              <label className="block text-sm font-medium text-gray-700">
+                Description
+              </label>
+              <textarea
+                placeholder="Enter project description"
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+                rows="4"
+                className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-200 p-2"
+                required
+              ></textarea>
+            </div>
+
+            {/* Due Date */}
+            <div>
+              <label className="block text-sm font-medium text-gray-700">
+                Due Date
+              </label>
+              <input
+                type="date"
+                value={dueDate}
+                onChange={(e) => setDueDate(e.target.value)}
+                className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-200 p-2"
+                required
+              />
+            </div>
+
+            {/* Submit Button */}
+            <div>
+              <button
+                type="submit"
+                className="w-full bg-blue-600 text-white py-3 px-4 rounded-md shadow hover:bg-blue-700 transition"
+              >
+                {loading ? "submitting..." : "Submit"}
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </DashboardLayout>
+  );
+}
---
+++ src/pages/Projects.jsx
@@ -0,0 +1,124 @@
+import React, { useEffect, useState } from "react";
+import DashboardLayout from "../components/DashboardLayout";
+import api from "../axios";
+import { Link } from "react-router-dom";
+
+export default function Projects() {
+    const [projects, setProjects] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const fetchProjects = async () => {
+            try {
+                const token = localStorage.getItem('token');
+                const response = await api.get('/projects', {
+                    headers : {
+                        Authorization: `Bearer ${token}`
+                    }
+                });
+
+                console.log(response.data.data);
+                setProjects(response.data.data);
+
+                console.log('Data type:', typeof projects);
+                console.log('Data value:', projects);
+                console.log('Is array:', Array.isArray(projects));
+
+            } catch (error) {
+                console.log("error fetching projects.");
+            } finally {
+                setLoading(false);
+            }
+        }
+
+        fetchProjects();
+    }, []);
+
+    // delete project by backend API
+    const handleDelete = async (id) => {
+        const confirmDelete = window.confirm("Are you sure to delete this project?");
+
+        if (! confirmDelete) return;
+
+        try {
+            const token = localStorage.getItem('token');
+
+            await api.delete(`/projects/${id}`, {
+                headers : {
+                    Authorization: `Bearer ${token}`,
+                }
+            });
+
+            // removing project from ui
+            setProjects(projects.filter((project) => project.id !== id));
+            alert('project deleted successfully.');
+        } catch (error) {
+            console.log("error to delete project", error);
+        }
+    }
+  return (
+    <DashboardLayout>
+      <div className="p-6 bg-gray-50 min-h-screen">
+        <h1 className="text-2xl font-semibold text-gray-800 mb-6">Projects</h1>
+
+        {loading ? (
+            <p className="text-gray-600">Loading projects...</p>
+        ) : projects.length === 0 ? (
+            <p className="text-gray-600">No projects found.</p>
+        ) : (
+            <div className="overflow-x-auto bg-white shadow-md rounded-lg">
+            <table className="min-w-full divide-y divide-gray-200">
+                <thead className="bg-gray-100">
+                <tr>
+                    <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                    S.No
+                    </th>
+                    <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                    Title
+                    </th>
+                    <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                    Description
+                    </th>
+                    <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                    Due Date
+                    </th>
+                    <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                    Actions
+                    </th>
+                </tr>
+                </thead>
+
+                <tbody className="divide-y divide-gray-200">
+                    {projects.map((project) => (
+                        <tr key="{project.id}" className="hover:bg-gray-50">
+                            <td className="px-6 py-4 whitespace-nowrap">{project.id}</td>
+                            <td className="px-6 py-4 whitespace-nowrap font-medium text-gray-900">
+                            {project.name}
+                            </td>
+                            <td className="px-6 py-4 whitespace-nowrap text-gray-700">
+                            {project.description}
+                            </td>
+                            <td className="px-6 py-4 whitespace-nowrap text-gray-700">
+                            {project.due_date}
+                            </td>
+                            <td className="px-6 py-4 whitespace-nowrap flex gap-2">
+                            <button className="px-3 py-1 bg-blue-500 text-white text-sm rounded hover:bg-blue-600 transition">
+                                View
+                            </button>
+                            <Link to={`/projects/edit/${project.id}`} className="px-3 py-1 bg-yellow-400 text-white text-sm rounded hover:bg-yellow-500 transition">
+                                Edit
+                            </Link>
+                            <button onClick={() => handleDelete(project.id)} className="px-3 py-1 bg-red-500 text-white text-sm rounded hover:bg-red-600 transition">
+                                Delete
+                            </button>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+            </div>
+        )}
+      </div>
+    </DashboardLayout>
+  );
+}
