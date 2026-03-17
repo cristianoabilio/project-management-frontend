ccbee3a262ae0f03bdd841766237e926a6cb947c
Cristiano Abilio <cristianocafr@gmail.com>
2026-03-17 12:27:59 -0300

    feat: Auth Service

--- src/services/authService.jsx
+++ src/services/authService.jsx
@@ -3,5 +3,5 @@ import api from "../axios";
 // handle login
 export const loginService = async (credentials) => {
     const response = await api.post('/login', credentials);
-    return response.data;
+    return response;
 }
